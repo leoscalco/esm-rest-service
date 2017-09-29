cc3a63d21d6402489199656c873790949d319c46
leoscalco <leonardo.scalco@hotmail.com>
2017-09-29 14:40:48 -0300

    Polimorfismo nas serializers

--- program_section/serializers.py
+++ program_section/serializers.py
@@ -2,7 +2,7 @@
 from rest_framework import serializers
 from program_section.models import *
 from user_section.serializers import ParticipantSerializer, ObserverSerializer
-from event_section.serializers import ActiveEventReadSerializer
+from event_section.serializers import ActiveEventReadSerializer, EventSerializer
 
 class ProgramWriteSerializer(serializers.ModelSerializer):
 
@@ -17,11 +17,11 @@ class ProgramReadSerializer(serializers.ModelSerializer):
 
     participants = ParticipantSerializer(many=True)
     observers = ObserverSerializer(many=True)
-    active_events = ActiveEventReadSerializer(many=True)
+    events = EventReadSerializer(many=True)
 
     class Meta:
         model = Program
         fields = (
             'id', 'title', 'description', 'starts', 'ends', 'updateDate',
-            'participants', 'observers', 'activeEvents'
+            'participants', 'observers', 'events'
         )
